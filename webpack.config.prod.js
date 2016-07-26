aba42fe8554ed2188cac2b008b5e761c9ee25c77
Andrew Ray <delvarworld@gmail.com>
2016-07-26 15:22:17 -0700

    oh yeah

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,6 +1,7 @@
 var path = require('path');
 var webpack = require('webpack');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: [
