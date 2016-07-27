7a68841966f3303f86480f57ca15cd52b384b881
Andrew Ray <delvarworld@gmail.com>
2016-07-27 16:17:01 -0700

    Version 2.0 test for twitter card

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,6 +2,8 @@ import React, { Component, } from 'react';
 import { alphabet, compounds, } from 'emoji-alphabet';
 import styles from './home.scss';
 
+const defaultString = 'Your Message Here';
+
 function getRandom( arr:Array ):any {
     return arr[ Math.floor( Math.random() * arr.length ) ];
 }
@@ -21,17 +23,29 @@ function getEmojiString( input:string, includeRandom:?boolean ):string {
         const value = alphabet[ key ];
         const isArray = value instanceof Array;
 
-        const emojiChar = includeRandom && isArray ?
-            getRandom( value ) : (
-                isArray ? value[ 0 ] : value
-            );
+        const chars = output.split( key );
 
-        output = output.split( key ).join( emojiChar );
+        if( chars.length > 1 ) {
+
+            for( let i = chars.length - 1; i > 0; i-- ) {
+
+                const emojiChar = includeRandom && isArray ?
+                    getRandom( value ) : (
+                        isArray ? value[ 0 ] : value
+                    );
+
+                chars.splice( i, 0, emojiChar );
+
+            }
+
+        }
+
+        output = chars.join('');
 
     }
 
     return output.split( / +/ )
-        .join( '&nbsp;&nbsp;&nbsp;&nbsp;' )
+        .join( '    ' )
         .replace( /F/g, 'Ⓕ' )
         .replace( /B/g, 'Ⓑ' )
         .replace( /K/g, 'Ⓚ' )
@@ -43,24 +57,48 @@ export default class Home extends Component {
 
     constructor() {
         super();
+
         this.onChange = this.onChange.bind( this );
+        this.onRandomizeToggle = this.onRandomizeToggle.bind( this );
+        this.onFocus = this.onFocus.bind( this );
+
         this.state = {
             value: '',
-            output: getEmojiString( 'Emoji Text Here' )
+            output: getEmojiString( defaultString, true ),
+            randomize: true,
         };
+    }
+
+    onFocus( event:Object ):void {
+
+        event.target.select();
+
     }
 
     onChange( event:Object ):void {
 
         this.setState({
             value: event.target.value,
-            output: getEmojiString( event.target.value )
+            output: getEmojiString( event.target.value || defaultString, this.state.randomize )
+        });
+
+    }
+
+    onRandomizeToggle( event:Object ):void {
+        this.setState({
+            randomize: event.target.checked
+        }, () => {
+
+            this.setState({
+                output: getEmojiString( this.state.value || defaultString, this.state.randomize )
+            });
+
         });
 
     }
 
     render() {
-        const { value, } = this.state;
+        const { value, randomize } = this.state;
 
         return <div>
             <img
@@ -71,21 +109,48 @@ export default class Home extends Component {
             />
             <textarea
                 className={ styles.input }
-                placeholder="Emoji Text Here"
+                placeholder={ defaultString }
                 onChange={ this.onChange }
                 type="text"
                 value={ this.state.value }
             />
-            <div
-                className={ styles.output }
-                dangerouslySetInnerHTML={{
-                    __html: this.state.output.replace( /\n/g, '<br />' )
-                }}
-            />
+            <label
+                className={ styles.label }
+            >
+                Randomize Emojis when there's more than one option?
+                <input
+                    type="checkbox"
+                    checked={ randomize }
+                    onChange={ this.onRandomizeToggle }
+                />
+            </label>
+            <div className={ styles.outputWrap }>
+                <div
+                    className={ styles.outputBlind }
+                    dangerouslySetInnerHTML={{
+                        __html: this.state.output
+                            .replace( / /g, '&nbsp;' )
+                            .replace( /\n/g, '<br />' )
+                    }}
+                />
+                <textarea
+                    onFocus={ this.onFocus }
+                    resizable={ false }
+                    className={ styles.output }
+                    placeholder="Emoji Text Here"
+                    onChange={ this.onChange }
+                    type="text"
+                    value={ this.state.output }
+                />
+            </div>
+            <div className={ styles.howTo }>
+                Click once to select, then copy!
+            </div>
             <div className={ styles.footer }>
                 <div className={ styles.footerContent }>
                     By <a href="https://twitter.com/andrewray" target="_blank">@andrewray</a> |{' '}
-                    <a href="https://github.com/DelvarWorld/emoji-ransom-generator" target="_blank">Github Repository</a>
+                    <a href="https://github.com/DelvarWorld/emoji-ransom-generator" target="_blank">Github Repository</a> |{' '}
+                    <a href="https://www.coinbase.com/andrewray" target="_blank">Buy me coffee? :)</a>
                 </div>
             </div>
         </div>;
