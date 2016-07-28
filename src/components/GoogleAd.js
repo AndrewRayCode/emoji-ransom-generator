e819ff0ed4bdb141972b641a176825b7eb1db13a
Andrew Ray <delvarworld@gmail.com>
2016-07-27 17:06:17 -0700

    more ad test

--- src/components/GoogleAd.js
+++ src/components/GoogleAd.js
@@ -11,14 +11,12 @@ export default class GoogleAd extends Component {
     }
 
     render() {
-        return <div>
-            <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js" />
-            <ins className="adsbygoogle"
-                style={{ display: 'block' }}
-                data-ad-client="ca-pub-1460131002512982"
-                data-ad-slot="7428902199"
-                data-ad-format="auto"></ins>
-        </div>;
+        return <ins className="adsbygoogle"
+            style={{ display: 'block' }}
+            data-ad-client="ca-pub-1460131002512982"
+            data-ad-slot="7428902199"
+            data-ad-format="auto"
+        />;
     }
 
 }
